b86fb65ded12fb8942ec3ec2611b2b818f40d511
ctphippin <ctphippin@umes.edu>
2026-03-26 21:38:06 -0400

    Create Login.jsx
    User login

---
+++ src/Login.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+
+    if (data.error) {
+      alert(data.error);
+    } else {
+      alert(`Welcome ${data.username}`);
+
+      // store user
+      localStorage.setItem("user", JSON.stringify(data));
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
